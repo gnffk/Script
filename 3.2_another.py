12bed402162b05c6d642523b8b0680db868eeedc
minsu <gnffk48@naver.com>
2024-03-13 10:39:49 +0900

    3.2 다른 방법

---
+++ 3.2_another.py
@@ -0,0 +1,11 @@
+import math
+x1,y1 = eval(input('첫번째 점 위도 경도:'))
+x2,y2 = eval(input('두번째 점 위도 경도:'))
+d = 6370.01*math.acos(math.sin(math.radians(x1))
+                      *math.sin(math.radians(x2))
+                      +math.cos(math.radians(x1))
+                      *math.cos(math.radians(x2))
+                      *math.cos(math.radians(y1-y2)))
+
+print('두점 사이 대권거리:',d)
+
