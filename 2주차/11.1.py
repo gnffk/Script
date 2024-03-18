3a56fd44eea7c1f03aa8ef6799e7fd7cb8a8ef0a
minsu <gnffk48@naver.com>
2024-03-18 19:12:33 +0900

    l에 리스트를 넣는다.

--- 2주차/11.1.py
+++ 2주차/11.1.py
@@ -4,6 +4,7 @@ def ex():
         s = input("3X4행렬의 행 {0}번 원소 입력:".format(i))
         l = [eval(i) for i in s.split()]
         matrix.append(l)
+    print(matrix)
     for j in range(4):
         print("열 {0}번 원소의 총합은 {1} 이다.".format(j,sumColumn(matrix,j)))
 def sumColumn(m,columnIndex):
