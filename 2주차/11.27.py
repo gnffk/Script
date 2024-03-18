70578bae8523208a85924e6f6067e5af0a63bdb9
minsu <gnffk48@naver.com>
2024-03-18 20:30:33 +0900

    열 정렬

--- 2주차/11.27.py
+++ 2주차/11.27.py
@@ -1,17 +1,31 @@
+#def sortColumns(m):
+#    m.sort()
+#    for i in range(len(m)):
+#        if i % 3 == 0 and i != 0:
+#            print()
+#        print(m[i], end=" ")
+
 def sortColumns(m):
-    m.sort()
-    for i in range(len(m)):
-        if i % 3 == 0 and i != 0:
-            print()
-        print(m[i], end=" ")
+    a = []
+    for j in range(3):
+        column = [m[i][j] for i in range(3)]
+        column.sort()
+        for i in range(3):
+            m[i][j] = column[i]
+
 
 def main():
-    print("3x3 행렬 하나씩 입력하시오: ")
+    print("3X3행렬의 행 원소 입력:")
     m = []
     for i in range(3):
-        l = input().split()
-        m.extend(l)
+        s = input()
+        l = [eval(i) for i in s.split()]
+        m.append(l)
     sortColumns(m)
+    for j in range(3):
+        for i in range(3):  # 정렬된 열 출력
+            print(m[j][i], end=" ")
+        print()
 
 
 main()
