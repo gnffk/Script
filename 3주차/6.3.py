0ffac43a0848f3e506c77eccf72cedfadd54e099
minsu <gnffk48@naver.com>
2024-03-20 10:41:15 +0900

    대칭수 판단 테스트 프로그램

--- 3주차/6.3.py
+++ 3주차/6.3.py
@@ -10,8 +10,10 @@ def reverse(number): # 3456 -> 6543
 def isPalindrome(number):
     if number != reverse(number):
         print('대칭수가 아님')
+        return True
     else:
         print('대칭수임')
+        return False
 
 def ex():
     number = eval(input("수를 입력하라"))
